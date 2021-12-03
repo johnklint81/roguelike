eaf65692c8e94cadc41c05e1d40201c68f892a76
John Klint <john.klint81@gmail.com>
2021-12-03 22:22:14 +0100

    12th update: Potions

--- actions.py
+++ actions.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from typing import Optional, Tuple, TYPE_CHECKING
 
 import color
+import exceptions
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Actor, Entity
+    from entity import Actor, Entity, Item
 
 
 class Action:
@@ -31,13 +32,33 @@ class Action:
         raise NotImplementedError()
 
 
+class ItemAction(Action):
+    def __init__(
+            self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+        if not target_xy:
+            target_xy = entity.x, entity.y
+            self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """Return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+
+    def perform(self) -> None:
+        """Invoke the item's ability, this action will be given to provide context."""
+        self.item.consumable.activate(self)
+
+
 class EscapeAction(Action):
     def perform(self) -> None:
         raise SystemExit()
 
 
 class WaitAction(Action):
-    def perform(self) ->None:
+    def perform(self) -> None:
         pass
 
 
@@ -71,7 +92,7 @@ class MeleeAction(ActionWithDirection):
     def perform(self) -> None:
         target = self.target_actor
         if not target:
-            return  # No entity to attack
+            raise exceptions.Impossible("Nothing to attack.")
 
         damage = self.entity.fighter.power - target.fighter.defense
 
@@ -97,11 +118,14 @@ class MovementAction(ActionWithDirection):
         dest_x, dest_y = self.dest_xy
 
         if not self.engine.game_map.in_bounds(dest_x, dest_y):
-            return  # Destination is out of bounds.
+            # Destination is out of bounds.
+            raise exceptions.Impossible("That way is blocked.")
         if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
-            return  # Destination is blocked by a tile.
+            # Destination is blocked by a tile.
+            raise exceptions.Impossible("That way is blocked.")
         if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return  # Destination is blocked by an entity.
+            # Destination is blocked by an entity.
+            raise exceptions.Impossible("That way is blocked.")
         self.entity.move(self.dx, self.dy)
 
 
