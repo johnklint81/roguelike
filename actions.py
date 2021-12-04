826beeae2fe5e431b6696e25f7ead775d11dc169
John Klint <john.klint81@gmail.com>
2021-12-04 14:05:13 +0100

    13th update: Potions, now working!

--- actions.py
+++ actions.py
@@ -32,15 +32,41 @@ class Action:
         raise NotImplementedError()
 
 
+class PickupAction(Action):
+    """Pickup an item and add it to the inventory, if there is room for it."""
+
+    def __init__(self, entity: Actor):
+        super().__init__(entity)
+
+    def perform(self) -> None:
+        actor_location_x = self.entity.x
+        actor_location_y = self.entity.y
+        inventory = self.entity.inventory
+
+        for item in self.engine.game_map.items:
+            if actor_location_x == item.x and actor_location_y == item.y:
+                if len(inventory.items) >= inventory.capacity:
+                    raise exceptions.Impossible("Your inventory is full.")
+
+                self.engine.game_map.entities.remove(item)
+                item.parent = self.entity.inventory
+                inventory.items.append(item)
+
+                self.engine.message_log.add_message(f"You picked up the {item.name}!")
+                return
+
+        raise exceptions.Impossible("There is nothing here to pick up.")
+
+
 class ItemAction(Action):
     def __init__(
-            self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
     ):
         super().__init__(entity)
         self.item = item
         if not target_xy:
             target_xy = entity.x, entity.y
-            self.target_xy = target_xy
+        self.target_xy = target_xy
 
     @property
     def target_actor(self) -> Optional[Actor]:
@@ -48,13 +74,13 @@ class ItemAction(Action):
         return self.engine.game_map.get_actor_at_location(*self.target_xy)
 
     def perform(self) -> None:
-        """Invoke the item's ability, this action will be given to provide context."""
+        """Invoke the items ability, this action will be given to provide context."""
         self.item.consumable.activate(self)
 
 
-class EscapeAction(Action):
+class DropItem(ItemAction):
     def perform(self) -> None:
-        raise SystemExit()
+        self.entity.inventory.drop(self.item)
 
 
 class WaitAction(Action):
--- components/consumable.py
+++ components/consumable.py
@@ -4,6 +4,7 @@ from typing import Optional, TYPE_CHECKING
 
 import actions
 import color
+import components.inventory
 from components.base_component import BaseComponent
 from exceptions import Impossible
 
@@ -24,19 +25,27 @@ class Consumable(BaseComponent):
         """
         raise NotImplementedError()
 
+    def consume(self) -> None:
+        """Remove the consumed item from its containing inventory."""
+        entity = self.parent
+        inventory = entity.parent
+        if isinstance(inventory, components.inventory.Inventory):
+            inventory.items.remove(entity)
+
 
 class HealingConsumable(Consumable):
     def __init__(self, amount: int):
         self.amount = amount
 
-        def activate(self, action: actions.ItemAction) -> None:
-            consumer = action.entity
-            amount_recovered = consumer.fighter.heal(self.amount)
+    def activate(self, action: actions.ItemAction) -> None:
+        consumer = action.entity
+        amount_recovered = consumer.fighter.heal(self.amount)
 
-            if amount_recovered > 0:
-                self.engine.message_log.add_message(
-                    f"You consume the {self.parent.name}, and recover {amount_recovered} HP!",
-                    color.health_recovered,
-                )
-            else:
-                raise Impossible(f"Your health is already full.")
+        if amount_recovered > 0:
+            self.engine.message_log.add_message(
+                f"You consume the {self.parent.name}, and recover {amount_recovered} HP!",
+                color.health_recovered,
+            )
+            self.consume()
+        else:
+            raise Impossible(f"Your health is already full.")
