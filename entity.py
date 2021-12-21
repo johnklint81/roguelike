cc21029978e35f2bc4b60e7fc28964a403966953
John Klint <john.klint81@gmail.com>
2021-12-21 23:28:20 +0100

    14th update: Scrolls!

--- entity.py
+++ entity.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import copy
+import math
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 
 from render_order import RenderOrder
@@ -69,6 +70,12 @@ class Entity:
             self.parent = gamemap
             gamemap.entities.add(self)
 
+    def distance(self, x: int, y: int) -> float:
+        """
+        Return the distance between the current entity and the given (x, y) coordinate.
+        """
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
         self.x += dx
@@ -110,6 +117,7 @@ class Actor(Entity):
     def is_alive(self) -> bool:
         """Returns True as long as this actor can perform actions."""
         return bool(self.ai)
+
 
 class Item(Entity):
     def __init__(
